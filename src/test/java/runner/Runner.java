9ba875199866397066fd884b4ed046a29ae3bc63
geetha <angu.geetha@gmail.com>
2023-05-04 22:39:09 -0400

    Ds Algo 1st commit

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,22 @@
+package runner;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		plugin = {"pretty", "html:target/dsalgo.html"}, //reporting purpose
+		monochrome=false,  //console output color
+		tags = "@tag", //tags from feature file
+		features = {"src/test/resources/feature"}, //location of feature files
+		glue= "stepdefinition") //location of step definition files
+public class Runner extends AbstractTestNGCucumberTests{
+	
+	@Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+				
+		return super.scenarios();
+    }
+}
