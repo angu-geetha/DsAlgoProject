78ca3d8e78237365d6396e96251f17c0662a4f54
Devimedidi <132529782+Devimedidi@users.noreply.github.com>
2023-05-07 13:01:42 -0500

    Signed-off-by: Devimedidi <132529782+Devimedidi@users.noreply.github.com>

--- src/test/java/portal/Homepage.java
+++ src/test/java/portal/Homepage.java
@@ -14,16 +14,16 @@ driver.get("https://dsportalapp.herokuapp.com");
 driver.findElement(By.linkText("Get Started")).click();
 driver.findElement(By.xpath("//a[normalize-space()='Register']")).click();
 driver.findElement(By.xpath("//input[@id='id_username']")).sendKeys("Sri@06");
- driver.findElement(By.xpath("//input[@id='id_password1']")).sendKeys("Medidi@2010");
- driver.findElement(By.xpath("//input[@id='id_password2']")).sendKeys("Medidi@2010");
- driver.findElement(By.xpath("//a[normalize-space()='Login']")).click();
- driver.findElement(By.xpath("//input[@id='id_username']")).sendKeys("Sri@06");
- driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("Medidi@2010");
- driver.findElement(By.xpath("//input[@value='Login']")).click();
- driver.findElement(By.xpath("//a[@href='data-structures-introduction']")).click();
-	driver.findElement(By.xpath("//a[normalize-space()='Time Complexity']")).click();
-	driver.findElement(By.xpath("//a[normalize-space()='Try here>>>']")).click();
-	driver.findElement(By.xpath("//*[@id=\"answer_form\"]/div/div/div[6]/div[1]/div/div/div/div[5]/div/pre")).sendKeys("print'Datastracture'");
+driver.findElement(By.xpath("//input[@id='id_password1']")).sendKeys("Medidi@2010");
+driver.findElement(By.xpath("//input[@id='id_password2']")).sendKeys("Medidi@2010");
+driver.findElement(By.xpath("//a[normalize-space()='Login']")).click();
+driver.findElement(By.xpath("//input[@id='id_username']")).sendKeys("Sri@06");
+driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("Medidi@2010");
+driver.findElement(By.xpath("//input[@value='Login']")).click();
+driver.findElement(By.xpath("//a[@href='data-structures-introduction']")).click();
+driver.findElement(By.xpath("//a[normalize-space()='Time Complexity']")).click();
+driver.findElement(By.xpath("//a[normalize-space()='Try here>>>']")).click();
+driver.findElement(By.xpath("//*[@id=\"answer_form\"]/div/div/div[6]/div[1]/div/div/div/div[5]/div/pre")).sendKeys("print'Datastracture'");
 	
  
  
