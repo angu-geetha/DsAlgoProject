63411575220be05d7bec0c4d22be59b6254e8ae3
pragymohapatra <pragymohapatra@localhost>
2023-05-10 21:49:57 -0400

    commit

---
+++ src/test/java/driverFactory/Driver_Factory.java
@@ -0,0 +1,13 @@
+package driverFactory;
+
+import org.openqa.selenium.WebDriver;
+
+public class Driver_Factory {
+
+	public static WebDriver driver;
+	
+	
+	
+}
+
+
--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,19 +1,24 @@
 package runner;
 
-import org.testng.annotations.DataProvider;
+//import org.junit.runner.RunWith;
+	import org.testng.annotations.DataProvider;
+	import io.cucumber.testng.AbstractTestNGCucumberTests;
+	import io.cucumber.testng.CucumberOptions;
+	//import io.cucumber.junit.Cucumber;
+		
+	//@RunWith(Cucumber.class) //Junit execution
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(
+	
+	@CucumberOptions(
 		plugin = {"pretty", "html:target/dsalgo.html"}, //reporting purpose
 		monochrome=false,  //console output color
 		tags = "@tag", //tags from feature file
 		features = {"src/test/resources/feature"}, //location of feature files
 		glue= "stepdefinition") //location of step definition files
+  
 public class Runner extends AbstractTestNGCucumberTests{
 	
-	@Override
+	@Override 
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
 				
